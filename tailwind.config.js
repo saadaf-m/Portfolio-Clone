bdae1d2f56959cdcab237d7cd229352ea24f49ac
Saadaf Mohsin <155853706+SaadafAlt@users.noreply.github.com>
2024-08-23 14:43:34 -0400

    translation of vanilla css of og proj into tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      // backgroundSize: {
+      //   'custom-bg-size': '100% 100vh',
+      // },
+      colors: {
+        // font colors
+        'sea-green': '#2EC57C',
+        'sage': '#6E9A8E',
+        'tiffany': '#95D1C1',
+        'slate-grey': 'C7C9C9',
+        'sage-transparent': 'rgba(214, 226, 223, 0.651)',
+        
+      },
+    },
   },
   plugins: [],
 }
