34b7808719242d1014526237a6160b4b8d3480d7
Saadaf Mohsin <155853706+SaadafAlt@users.noreply.github.com>
2024-08-24 23:13:02 -0400

    continue to apply styles to home section
    - note that when applying custom class to a custom query, tailwind might act up.
    - solution: add the custom class within the same layer you are applying styles to whatver class then it works

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
   theme: {
     screens: {
       'xs': '360px',  // => @media (min-width: 360px) {...}
+      's': '480px',  // => @media (min-width: 480px) {...}
     },
     extend: {
       colors: {
